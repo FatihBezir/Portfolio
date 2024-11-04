07c5b8106fda6179f8d5aa63b1cb5fcc6fd2ac67
unknown <fatihbezir@hotmail.com>
2024-11-04 18:59:41 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,19 @@ import BackgroundImage from "./components/BackgroundImage";
 import TerminalBox from "./components/TerminalBox";
 
 export default function App() {
-    return (
-        <>
-            <div className="h-screen w-full flex justify-center items-center font-josefin relative">
-                <div className="w-full">
-                    <div className="block sm:hidden mb-10 bg-yellow-200 py-3 text-center text-xl text-gray-700 hidden">
-                        <p>For better experience please try this with pc!</p>
-                        <p>This is not design for mobile view.</p>
-                        <a href="https://M.Fatih_BEZİR.me" className="block p-2 underline">
-                            Visit this instead
-                        </a>
-                    </div>
-                    <TerminalBox />
-                </div>
-                <BackgroundImage />
-            </div>
-        </>
-    );
+  return (
+    <div className="min-h-screen flex justify-center items-center font-josefin relative">
+      <div className="w-full">
+        <div className="block sm:hidden mb-10 bg-yellow-200 py-3 text-center text-xl text-gray-700 hidden">
+          <p>For better experience please try this with pc!</p>
+          <p>This is not design for mobile view.</p>
+          <a href="https://M.Fatih_BEZİR.me" className="block p-2 underline">
+            Visit this instead
+          </a>
+        </div>
+        <TerminalBox />
+      </div>
+      <BackgroundImage />
+    </div>
+  );
 }
