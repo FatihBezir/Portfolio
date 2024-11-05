d4c17f2d75a1159634f082bf02b78f7fb1782abc
unknown <fatihbezir@hotmail.com>
2024-11-05 09:49:58 +0300

    .

--- src/index.tsx
+++ src/index.tsx
@@ -2,26 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { I18nextProvider } from 'react-i18next';
-import i18n from 'i18next';
+import { I18nextProvider } from "react-i18next";
+import i18n from "i18next";
 
 i18n.init({
-    resources: {
-        en: { translation: require('./locales/en.json') },
-        tr: { translation: require('./locales/tr.json') }
-    },
-    lng: "tr", // Set default language to Turkish
-    fallbackLng: "en",
-    interpolation: { escapeValue: false }
+  resources: {
+    en: { translation: require("./locales/en.json") },
+    tr: { translation: require("./locales/tr.json") },
+  },
+  lng: "tr", // Set default language to Turkish
+  fallbackLng: "en",
+  interpolation: { escapeValue: false },
 });
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-	<I18nextProvider i18n={i18n}>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
-	</I18nextProvider>
+  <>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <I18nextProvider i18n={i18n}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </I18nextProvider>
+  </>
 );
